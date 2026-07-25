3a39215d5fe14f32dbf1b7bc44abcb22015ea6c4
jared-bidlow <170748436+jared-bidlow@users.noreply.github.com>
2026-07-24 22:31:57 -0400

    initial: @wornpage/undo v0.1.0

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as UndoReceipt } from './UndoReceipt.svelte';
+export { default as UndoElement } from './UndoElement.svelte';
+export type { UndoAction, UndoStore } from './types.js';
+export { MAX_UNDO, UNDO_LABELS } from './types.js';
